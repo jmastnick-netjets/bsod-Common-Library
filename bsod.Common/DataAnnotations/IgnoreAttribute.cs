d48256c61b42270376c69afacc42d03febbf9dd3
Jason Mastnick <jmastnick@netjets.com>
2023-02-09 09:46:25 -0500

    Added Ignore Attribute

---
+++ bsod.Common/DataAnnotations/IgnoreAttribute.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bsod.Common.DataAnnotations
+{
+    public class IgnoreAttribute : Attribute
+    {
+        public bool ShowTable { get; set; }
+        public bool ShowList { get; set; }
+        public bool ShowExport { get; set; }
+        public IgnoreAttribute()
+        {
+
+        }
+    }
+}
